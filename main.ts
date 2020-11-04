52bb6ed1044e9e628509069d06b71d9ceee0fb5f
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2020-11-05 00:22:44 +0100

    First clunky version

--- main.ts
+++ main.ts
@@ -1,32 +1,74 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { execSync } from 'child_process';
 
 export default class MyPlugin extends Plugin {
-	onload() {
+	public settings: TemplaterSettings;
+
+	async onload() {
+		this.settings = (await this.loadData()) || new TemplaterSettings();
+
 		console.log('loading plugin');
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
+		
+		/*
+		exec("ls -la", (error, stdout, stderr) => {
+		    if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		    }
+		    if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		    }
+		    console.log(`stdout: ${stdout}`);
 		});
+		*/
+		
+		this.addRibbonIcon('dice', 'Templater', async () => {
+			console.log(this.settings);
 
-		this.addStatusBarItem().setText('Status Bar Text');
+			let files = this.app.vault.getFiles();
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
+			let file = files[0];
+
+			let i = 1;
+
+			let content = await this.app.vault.read(file);
+
+			for (let template in this.settings.templates) {	
+				let cmd = this.settings.templates[template];
+
+				if (template === "" || cmd === "") {
+					continue;
 				}
-				return false;
+
+				console.log("i:",i);
+				if (content.contains(template)) {
+					try {
+						let stdout = execSync(cmd);
+						console.log(stdout);
+						console.log(stdout.toString());
+
+						content = content.replace(
+							template, 
+							stdout.toString()
+						);
+						console.log("ii:",i);
+						this.app.vault.modify(file, content);
+					}
+					catch (error) {
+						new Notice('ERROR !');
+						console.log(`error: ${error.message}`);
+					}
+				
+					i += 1;
+				}
 			}
+
 		});
+
+		this.addStatusBarItem().setText('TEST STATUS BAR TEXT');
+
+		//new SampleModal(this.app).open();
 
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
@@ -41,8 +83,8 @@ export default class MyPlugin extends Plugin {
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-		console.log('unloading plugin');
+	async onunload() {
+		await this.saveData(this.settings);
 	}
 }
 
@@ -53,31 +95,76 @@ class SampleModal extends Modal {
 
 	onOpen() {
 		let {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('TEST WHOAH!');
 	}
 
 	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
+		
 	}
+}
+
+class TemplaterSettings {
+	templates: {[template: string]: string} = {"": ""};
 }
 
 class SampleSettingTab extends PluginSettingTab {
 	display(): void {
+		const plugin: MyPlugin = (this as any).plugin;
 		let {containerEl} = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		let i = 1;
+
+		for (let template in plugin.settings.templates) {
+			let old_value = template;
+			let cmd = plugin.settings.templates[template];
+
+			new Setting(containerEl)
+				.setName('Template ' + i)
+				.addText(text => {
+						text.setPlaceholder('Template')
+						.setValue(template)
+						.onChange((value) => {
+							cmd = plugin.settings.templates[old_value];
+							console.log("cmd", cmd);
+							delete plugin.settings.templates[old_value];
+							console.log("old_value:",old_value);
+
+							if (value !== "") {
+								console.log("new_value:",value);
+								plugin.settings.templates[value] = cmd;
+								old_value = value;
+							}
+							else {
+								// Force refresh
+								this.display();
+							}
+							
+							plugin.saveData(plugin.settings);
+							console.log(plugin.settings);
+						})
+					}
+				)
+				.addText(text => text.setPlaceholder('Command')
+					.setValue(cmd)
+					.onChange((value) => {
+						plugin.settings.templates[old_value] = value;
+						plugin.saveData(plugin.settings);
+						console.log(plugin.settings);
+					})
+				);
+
+				i+=1;
+		}
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange((value) => {
-					console.log('Secret: ' + value);
-				}));
-
+			.addButton(button => {
+				button.setButtonText("Add Template").onClick(() => {
+					plugin.settings.templates[""] = "";
+					// Force refresh
+					this.display();
+				})
+			});
 	}
 }
