ec17c9417325645d0f831e55424c8bd8d73e25a6
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2021-07-31 22:29:38 +0200

    refactor: cleaning files and switching to pnpm

--- src/TemplaterEditor.ts
+++ src/TemplaterEditor.ts
@@ -4,6 +4,8 @@ import { TemplaterError } from "Error";
 
 import "mode/javascript";
 import "mode/custom_overlay";
+import "mode/show-hint";
+import { delay } from "Utils";
 
 const TP_CMD_TOKEN_CLASS: string = "templater-command";
 const TP_INLINE_CLASS: string = "templater-inline";
@@ -20,6 +22,7 @@ export class TemplaterEditor {
 
     async setup(): Promise<void> {
         await this.registerCodeMirrorMode();
+        //await this.registerHinter();
     }
 
 	async registerCodeMirrorMode(): Promise<void> {
@@ -130,4 +133,56 @@ export class TemplaterEditor {
             return overlay_mode(window.CodeMirror.getMode(config, "hypermd"), templaterOverlay);
 		}); 
 	}
+
+    async registerHinter(): Promise<void> {
+        // TODO
+        /*
+        await delay(1000);
+
+        var comp = [
+            ["here", "hither"],
+            ["asynchronous", "nonsynchronous"],
+            ["completion", "achievement", "conclusion", "culmination", "expirations"],
+            ["hinting", "advise", "broach", "imply"],
+            ["function","action"],
+            ["provide", "add", "bring", "give"],
+            ["synonyms", "equivalents"],
+            ["words", "token"],
+            ["each", "every"],
+        ];
+    
+        function synonyms(cm: any, option: any) {
+            return new Promise(function(accept) {
+                setTimeout(function() {
+                    var cursor = cm.getCursor(), line = cm.getLine(cursor.line)
+                    var start = cursor.ch, end = cursor.ch
+                    while (start && /\w/.test(line.charAt(start - 1))) --start
+                    while (end < line.length && /\w/.test(line.charAt(end))) ++end
+                    var word = line.slice(start, end).toLowerCase()
+                    for (var i = 0; i < comp.length; i++) {
+                        if (comp[i].indexOf(word) != -1) {
+                            return accept({
+                                list: comp[i],
+                                from: window.CodeMirror.Pos(cursor.line, start),
+                                to: window.CodeMirror.Pos(cursor.line, end)
+                            });
+                        }
+                    }
+                    return accept(null);
+                }, 100)
+            });
+        }
+
+        this.app.workspace.on("codemirror", cm => {
+            cm.setOption("extraKeys", {"Ctrl-Space": "autocomplete"});
+            cm.setOption("hintOptions", {hint: synonyms});
+        });
+
+        this.app.workspace.iterateCodeMirrors(cm => {
+            console.log("CM:", cm);
+            cm.setOption("extraKeys", {"Space": "autocomplete"});
+            cm.setOption("hintOptions", {hint: synonyms});
+        });
+        */
+    }
 }
