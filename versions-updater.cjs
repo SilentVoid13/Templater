20d8458db14bd8443eb49e27e2e904d16af1737b
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2022-01-14 14:03:16 +0100

    refactor: updating packages and downgrading standard-version packages to keep them working

--- versions-updater.cjs
+++ versions-updater.cjs
@@ -1,12 +1,15 @@
 const stringifyPackage = require("stringify-package");
 const detectIndent = require("detect-indent");
 const detectNewline = require("detect-newline");
+//import * as stringifyPackage from "stringify-package"
+//import * as detectIndent from "detect-indent"
+//import * as detectNewline from "detect-newline"
 
 module.exports.readVersion = function (contents) {
     return Object.keys(JSON.parse(contents))[0];
 };
 
-module.exports.writeVersion = function (contents, version) {
+module.exports.writeVersion = async function (contents, version) {
     let json = JSON.parse(contents);
     let indent = detectIndent(contents).indent;
     let newline = detectNewline(contents);
