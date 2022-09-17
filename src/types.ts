08792f8ec23f0dbc04460a420df98976d69421aa
Sam <sam@shbgm.ca>
2022-09-17 16:22:51 -0400

    fix: errors after merging PRs (#832)

--- src/types.ts
+++ src/types.ts
@@ -5,12 +5,19 @@ declare module "obsidian" {
 
     interface Vault {
         getConfig: (key: string) => string;
+        exists: (path: string) => Promise<boolean>;
     }
     interface FileManager {
         createNewMarkdownFile: (
             folder: TFolder | undefined,
             filename: string
         ) => Promise<TFile>;
+    }
+    interface DataAdapter {
+        basePath: string;
+        fs: {
+            uri: string;
+        };
     }
 }
 
