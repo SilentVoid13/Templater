79e2925f495cba50a4b1033a3caaaac423fbb6f1
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2022-11-08 16:37:13 +0100

    feat: cleaning package, preparing for release

--- src/core/parser/Parser.ts
+++ src/core/parser/Parser.ts
@@ -1,29 +1,22 @@
-import init, {ParserConfig, Renderer} from '@silentvoid13/rusty_engine';
+import init, { ParserConfig, Renderer } from "@silentvoid13/rusty_engine";
 
+// TODO: find a cleaner way to embed wasm
 // @ts-ignore
-import {
-  default as wasmbin
-} from '../../../node_modules/@silentvoid13/rusty_engine/rusty_engine_bg.wasm';
+import { default as wasmbin } from "../../../node_modules/@silentvoid13/rusty_engine/rusty_engine_bg.wasm";
 
 export class Parser {
-  private renderer: Renderer;
+    private renderer: Renderer;
 
-  async init() {
-    await init(wasmbin);
-    const config = new ParserConfig(
-        "<%",
-        "%>",
-        '\0',
-        '*',
-        '-',
-        '_',
-        "tR",
-    );
-    this.renderer = new Renderer(config);
-  }
+    async init() {
+        await init(wasmbin);
+        const config = new ParserConfig("<%", "%>", "\0", "*", "-", "_", "tR");
+        this.renderer = new Renderer(config);
+    }
 
-  async parse_commands(content: string,
-                       context: Record<string, unknown>): Promise<string> {
-    return this.renderer.render_content(content, context);
-  }
+    async parse_commands(
+        content: string,
+        context: Record<string, unknown>
+    ): Promise<string> {
+        return this.renderer.render_content(content, context);
+    }
 }
