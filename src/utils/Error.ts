de54d0cbc30e63a4434d914160ffabe26d7f5474
Zachatoo <Zachatoo@users.noreply.github.com>
2023-12-01 20:55:18 -0700

    fix: do not stack trace using Error.captureStackTrace in environments that don't support it (iOS)

--- src/utils/Error.ts
+++ src/utils/Error.ts
@@ -4,7 +4,9 @@ export class TemplaterError extends Error {
     constructor(msg: string, public console_msg?: string) {
         super(msg);
         this.name = this.constructor.name;
-        Error.captureStackTrace(this, this.constructor);
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor);
+        }
     }
 }
 
