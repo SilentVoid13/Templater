4646c973e23ec3b99f63c908c663c8348a7e754a
Zachatoo <Zachatoo@users.noreply.github.com>
2025-07-12 20:56:33 -0600

    fix: Security issue with using innerHTML

--- src/utils/Log.ts
+++ src/utils/Log.ts
@@ -3,20 +3,29 @@ import { TemplaterError } from "./Error";
 
 export function log_update(msg: string): void {
     const notice = new Notice("", 15000);
-    // TODO: Find better way for this
-    // @ts-ignore
-    notice.noticeEl.innerHTML = `<b>Templater update</b>:<br/>${msg}`;
+    const messageEl = createFragment((frag) => {
+        frag.createEl("b", { text: "Templater update" });
+        frag.createEl("span", { text: ":" });
+        frag.createEl("br");
+        frag.createEl("span", { text: msg });
+    });
+    notice.noticeEl.appendChild(messageEl);
 }
 
 export function log_error(e: Error | TemplaterError): void {
     const notice = new Notice("", 8000);
-    if (e instanceof TemplaterError && e.console_msg) {
-        // TODO: Find a better way for this
-        // @ts-ignore
-        notice.noticeEl.innerHTML = `<b>Templater Error</b>:<br/>${e.message}<br/>Check console for more information`;
-        console.error(`Templater Error:`, e.message, "\n", e.console_msg);
-    } else {
-        // @ts-ignore
-        notice.noticeEl.innerHTML = `<b>Templater Error</b>:<br/>${e.message}`;
-    }
+    const messageEl = createFragment((frag) => {
+        frag.createEl("b", { text: "Templater Error" });
+        frag.createEl("span", { text: ":" });
+        frag.createEl("br");
+        frag.createEl("span", { text: e.message });
+        if (e instanceof TemplaterError && e.console_msg) {
+            frag.createEl("br");
+            frag.createEl("span", {
+                text: "Check console for more information",
+            });
+            console.error(`Templater Error:`, e.message, "\n", e.console_msg);
+        }
+    });
+    notice.noticeEl.appendChild(messageEl);
 }
