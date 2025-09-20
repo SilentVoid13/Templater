e9b0d2820ed53e71007522d1a14bef6ea034228d
Zachatoo <Zachatoo@users.noreply.github.com>
2025-09-20 00:33:01 -0600

    feat: add `tp.system.multi_suggester`
    refs: #461, #1130

---
+++ src/core/functions/internal_functions/system/MultiSuggesterModal.ts
@@ -0,0 +1,173 @@
+import { TemplaterError } from "utils/Error";
+import {
+    AbstractInputSuggest,
+    App,
+    ButtonComponent,
+    Modal,
+    prepareFuzzySearch,
+    setIcon,
+    TextComponent,
+} from "obsidian";
+
+export class MultiSuggesterModal<T> extends Modal {
+    private resolve: (values: T[]) => void;
+    private reject: (reason?: TemplaterError) => void;
+    private submitted = false;
+    private selectedItems: T[] = [];
+    private listEl: HTMLDivElement;
+    private inputComponent: TextComponent;
+    private suggester: InstanceType<typeof Suggester>;
+
+    constructor(
+        app: App,
+        private text_items: string[] | ((item: T) => string),
+        private items: T[],
+        placeholder: string,
+        limit?: number
+    ) {
+        super(app);
+        this.listEl = this.contentEl.createDiv("templater-multisuggester-list");
+        const inputContainer = this.contentEl.createDiv(
+            "templater-multisuggester-div"
+        );
+        this.inputComponent = new TextComponent(inputContainer).setPlaceholder(
+            placeholder
+        );
+        this.inputComponent.inputEl.addClass("templater-multisuggester-input");
+        this.suggester = new Suggester(
+            app,
+            this.inputComponent.inputEl,
+            this.getItemText.bind(this),
+            items,
+            limit
+        ).onSelect(this.onChooseItem.bind(this));
+        const buttonContainer = this.contentEl.createDiv(
+            "modal-button-container"
+        );
+        new ButtonComponent(buttonContainer)
+            .setButtonText("Save")
+            .setCta()
+            .onClick(() => this.save());
+        new ButtonComponent(buttonContainer)
+            .setButtonText("Cancel")
+            .onClick(() => this.close());
+    }
+
+    onOpen(): void {
+        this.display();
+    }
+
+    display(): void {
+        this.listEl.empty();
+        this.selectedItems.forEach((item) => {
+            const itemEl = this.listEl.createDiv("mobile-option-setting-item");
+            itemEl
+                .createSpan("mobile-option-setting-item-name")
+                .setText(this.getItemText(item));
+            itemEl.createDiv(
+                "clickable-icon mobile-option-setting-item-option-icon",
+                (deleteEl) => {
+                    setIcon(deleteEl, "lucide-x");
+                    deleteEl.addEventListener("click", () => {
+                        this.onRemoveItem(item);
+                    });
+                }
+            );
+            // itemEl.createDiv("clickable-icon mobile-option-setting-item-option-icon mobile-option-setting-drag-icon", dragEl => {
+            //     setIcon(dragEl, "lucide-menu");
+
+            // })
+        });
+    }
+
+    getItemText(item: T): string {
+        if (this.text_items instanceof Function) {
+            return this.text_items(item);
+        }
+        return (
+            this.text_items[this.items.indexOf(item)] || "Undefined Text Item"
+        );
+    }
+
+    onChooseItem(item: T): void {
+        this.selectedItems.push(item);
+        const filteredItems = this.items.filter((item) => {
+            return !this.selectedItems.some(
+                (selected_item) => selected_item === item
+            );
+        });
+        this.suggester.setItems(filteredItems);
+        this.display();
+    }
+
+    onRemoveItem(item: T): void {
+        this.selectedItems = this.selectedItems.filter(
+            (selectedItem) => selectedItem !== item
+        );
+        const filteredItems = this.items.filter((item) => {
+            return !this.selectedItems.some(
+                (selected_item) => selected_item === item
+            );
+        });
+        this.suggester.setItems(filteredItems);
+        this.display();
+    }
+
+    save(): void {
+        this.submitted = true;
+        this.close();
+        this.resolve(this.selectedItems);
+    }
+
+    onClose(): void {
+        if (!this.submitted) {
+            this.reject(new TemplaterError("Cancelled prompt"));
+        }
+    }
+
+    async openAndGetValue(
+        resolve: (values: T[]) => void,
+        reject: (reason?: TemplaterError) => void
+    ): Promise<void> {
+        this.resolve = resolve;
+        this.reject = reject;
+        this.open();
+    }
+}
+
+class Suggester<T> extends AbstractInputSuggest<T> {
+    constructor(
+        app: App,
+        private textInputEl: HTMLInputElement | HTMLDivElement,
+        private getItemText: (item: T) => string,
+        private items: T[],
+        limit?: number
+    ) {
+        super(app, textInputEl);
+        limit && (this.limit = limit);
+    }
+    protected getSuggestions(query: string): T[] | Promise<T[]> {
+        const q = prepareFuzzySearch(query);
+        return this.items.reduce((acc, item) => {
+            const itemText = this.getItemText(item);
+            if (q(itemText)) {
+                acc.push(item);
+            }
+            return acc;
+        }, [] as T[]);
+    }
+
+    renderSuggestion(value: T, el: HTMLElement): void {
+        el.createDiv("suggestion-content").setText(this.getItemText(value));
+    }
+
+    setItems(items: T[]) {
+        this.items = items;
+    }
+
+    selectSuggestion(value: T, evt: MouseEvent | KeyboardEvent) {
+        this.setValue("");
+        this.close();
+        super.selectSuggestion(value, evt);
+    }
+}
