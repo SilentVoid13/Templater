7e254f0de09a078ec6152ec0566a357bc32304ad
Zachatoo <Zachatoo@users.noreply.github.com>
2026-05-01 08:54:28 -0600

    tests: update hooks tests to new paradigm

--- test/specs/internal-modules/hooks.e2e.ts
+++ test/specs/internal-modules/hooks.e2e.ts
@@ -1,42 +1,37 @@
-import { resetWorkspace } from "../../helpers/obsidianTestHelpers";
-import {
-    arePropertiesVisible,
-    deleteVaultPath,
-    runInNewLeafAndGetOutput,
-    uniqueTestName,
-} from "../../helpers/legacyTemplaterTestHelpers";
+import { obsidianPage } from "wdio-obsidian-service";
+import OpenInsertTemplateModalPage from "../../page-objects/OpenInsertTemplateModal.page";
+import WorkspacePage from "../../page-objects/Workspace.page";
+import EmptyStateViewPage from "../../page-objects/EmptyStateView.page";
+import VaultPage from "../../page-objects/Vault.page";
+import ActiveMarkdownViewPage from "../../page-objects/ActiveMarkdownView.page";
 
 describe("InternalModuleHooks", () => {
-    const cleanupPaths: string[] = [];
-
-    beforeEach(async () => {
-        cleanupPaths.length = 0;
-        await resetWorkspace();
-    });
-
-    afterEach(async () => {
-        for (const path of cleanupPaths.reverse()) {
-            await deleteVaultPath(path);
-        }
-    });
-
     it("tp.hooks.on_all_templates_executed shows properties in live preview", async () => {
-        const baseName = uniqueTestName("wdio-hooks");
-        const targetPath = `${baseName}-target.md`;
-        const templatePath = `${baseName}-template.md`;
-        cleanupPaths.push(targetPath, templatePath);
-
-        const templateContent = `<%*\ntp.hooks.on_all_templates_executed(async () => {\n  const file = tp.file.find_tfile(tp.file.path(true));\n  await app.fileManager.processFrontMatter(file, (frontmatter) => {\n    frontmatter["key"] = "value";\n  });\n});\n%>\nTEXT THAT SHOULD STAY`;
-
-        const output = await runInNewLeafAndGetOutput({
-            templatePath,
-            targetPath,
-            templateContent,
-            targetContent: "",
-            waitCache: true,
+        const templateContent =
+            "<%*\n" +
+            "tp.hooks.on_all_templates_executed(async () => {\n" +
+            "  const file = tp.file.find_tfile(tp.file.path(true));\n" +
+            "  await app.fileManager.processFrontMatter(file, (frontmatter) => {\n" +
+            '    frontmatter["key"] = "value";\n' +
+            "  });\n" +
+            "});\n" +
+            "%>\n" +
+            "TEXT THAT SHOULD STAY";
+        await obsidianPage.resetVault("test/vault", {
+            "templates/tp.hooks.on_all_templates_executed.md": templateContent,
         });
-
-        expect(await arePropertiesVisible()).toBe(true);
-        expect(output).toBe("---\nkey: value\n---\n\nTEXT THAT SHOULD STAY");
+        await obsidianPage.loadWorkspaceLayout("empty");
+        await EmptyStateViewPage.clickCreateNewNote();
+        await WorkspacePage.expectActiveTabToHaveText("Untitled");
+        await OpenInsertTemplateModalPage.open();
+        await OpenInsertTemplateModalPage.selectSuggestionByName(
+            "tp.hooks.on_all_templates_executed",
+        );
+        await WorkspacePage.waitForAllTemplatesExecuted();
+        await ActiveMarkdownViewPage.expectPropertiesToBeVisible();
+        await VaultPage.expectFileToHaveContent(
+            "Untitled.md",
+            "---\nkey: value\n---\n\nTEXT THAT SHOULD STAY",
+        );
     });
 });
